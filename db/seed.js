b303721f210f77a70072dbf7309cd7b87408cc70
Keith <keith@Keiths-MBP.Home>
2025-09-17 13:54:37 +0100

    more work on seed function

--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,13 @@
 const db = require("./db-connection-pool");
 
 async function seed() {
+
+  await db.query(`DROP TABLE property_types;`);
+
   await db.query(`CREATE TABLE property_types(
                 property_type VARCHAR PRIMARY KEY NOT NULL,
                 description TEXT)`);
-  console.log("Success!");
+
 }
 
 module.exports = seed;
