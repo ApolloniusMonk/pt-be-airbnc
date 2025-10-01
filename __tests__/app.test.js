11710cc4e2e155385f0d906da6bf6ff76ac45a98
ADMIN <admin@iMac.broadband>
2025-10-01 13:29:15 +0100

    working on first ticket for properties

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,7 +10,7 @@ const {
 const db = require("../db/db-connection-pool");
 
 beforeEach(async () => {
-  await seed(reviewsData, propertiesData, usersData, propertyTypesData);
+  await seed(propertyTypesData, usersData, propertiesData, reviewsData);
 });
 
 afterAll(() => {
@@ -20,7 +20,29 @@ afterAll(() => {
 describe("app", () => {
   describe("GET api/properties", () => {
     test("responds with status of 200", async () => {
-      const response = await request(app).get("/api/properties").expect(200);
+      await request(app).get("/api/properties").expect(200);
     });
+  });
+  test("responds with an array on the key of properties", async () => {
+    const { body } = await request(app).get("/api/properties");
+    console.log(body);
+
+    expect(Array.isArray(body.properties)).toBe(true);
+  });
+  test("respnds with correct number of properties", async () => {
+    const { body } = await request(app).get("/api/properties");
+
+    expect(body.properties).toHaveLength(propertiesData.length);
+  });
+  test("each property has correct keys", async () => {
+    const { body } = await request(app).get("/api/properties");
+
+    expect(body.properties[0].property_id).toBe(1);
+    expect(body.properties[0].property_name).toBe(
+      "Modern Apartment in City Center"
+    );
+    expect(body.properties[0].location).toBe("London, UK");
+    expect(body.properties[0].price_per_night).toBe(120.0);
+    expect(body.properties[0].host).toBe("Alice Johnson");
   });
 });
--- app.js
+++ app.js
@@ -3,8 +3,21 @@ const db = require("./db/db-connection-pool");
 
 const app = express();
 
-app.get("/api/properties", (req, res, next) => {
-  res.status(200).send();
+app.use(express.json());
+
+app.get("/api/properties", async (req, res, next) => {
+  const query = `SELECT 
+                 property_id,
+                 name AS property_name,
+                 location,
+                 price_per_night,
+                 host_name AS host
+                 FROM properties;
+
+                 `;
+  await db.query(query).then((result) => {
+    res.status(200).send({ properties: result.row });
+  });
 });
 
 module.exports = app;
