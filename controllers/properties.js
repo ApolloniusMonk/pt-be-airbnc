3afe4ad4f1fb4083d45a283aa84ba33b6e666b1e
ADMIN <admin@iMac.broadband>
2025-10-09 15:38:02 +0100

    happy path for property_type query complete

--- controllers/properties.js
+++ controllers/properties.js
@@ -1,6 +1,7 @@
 const { fetchProperties } = require("../models/properties");
 
 exports.getProperties = async (req, res, next) => {
-  const properties = await fetchProperties();
+  const { property_type } = req.query;
+  const properties = await fetchProperties(property_type);
   res.status(200).send({ properties });
 };
--- models/properties.js
+++ models/properties.js
@@ -1,18 +1,25 @@
 const db = require("../db/db-connection-pool");
 
-exports.fetchProperties = async () => {
-  const query = `SELECT 
+exports.fetchProperties = async (propertyType) => {
+  let query = `SELECT 
                  properties.property_id,
                  properties.name AS property_name,
                  properties.location,
                  properties.price_per_night,
+                 property_types.property_type,
                  users.first_name || ' ' || users.surname AS host 
                  FROM properties
-                 JOIN users ON properties.host_id = users.user_id;
+                 JOIN users ON properties.host_id = users.user_id
+                 JOIN property_types ON properties.property_type = property_types.property_type
 
                 `;
+  const queryValues = [];
 
-  const { rows } = await db.query(query);
+  if (propertyType) {
+    query += `WHERE properties.property_type = $1;`;
+    queryValues.push(propertyType);
+  }
+  const { rows } = await db.query(query, queryValues);
 
   return rows;
 };
