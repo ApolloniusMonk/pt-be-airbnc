22a965a21e0197ba1551099b63d5899a9080d399
ADMIN <admin@iMac.local>
2025-12-15 15:29:10 +0000

    changed line 14 to COALESCE(ARRAY_AGG(i.image_url ORDER BY i.image_id) FILTER (WHERE i.image_url IS NOT NULL), '{}') AS images

--- models/properties.js
+++ models/properties.js
@@ -11,7 +11,7 @@ exports.fetchProperties = async (filters = {}) => {
       p.price_per_night,
       pt.property_type,
       u.first_name || ' ' || u.surname AS host,
-      COALESCE(ARRAY_AGG(i.image_url ORDER BY i.image_url DESC) FILTER (WHERE i.image_url IS NOT NULL), '{}') AS images
+      COALESCE(ARRAY_AGG(i.image_url ORDER BY i.image_id) FILTER (WHERE i.image_url IS NOT NULL), '{}') AS images
     FROM properties p
     JOIN users u ON p.host_id = u.user_id
     JOIN property_types pt ON p.property_type = pt.property_type
@@ -96,14 +96,14 @@ exports.fetchPropertyById = async (id) => {
       p.price_per_night,
       p.description,
       u.first_name || ' ' || u.surname AS host,
-      COALESCE(ARRAY_AGG(i.image_url ORDER BY i.image_url ASC) FILTER (WHERE i.image_url IS NOT NULL), '{}') AS images,
+      COALESCE(ARRAY_AGG(i.image_url ORDER BY i.image_id) FILTER (WHERE i.image_url IS NOT NULL), '{}') AS images,
       COUNT(f.favourite_id)::INT AS favourite_count
     FROM properties p
     JOIN users u ON p.host_id = u.user_id
     LEFT JOIN images i ON i.property_id = p.property_id
     LEFT JOIN favourites f ON f.property_id = p.property_id
     WHERE p.property_id = $1
-    GROUP BY p.property_id, u.first_name, u.surname, i.image_url;
+    GROUP BY p.property_id, u.first_name, u.surname;
   `;
 
   const { rows } = await db.query(query, [id]);
@@ -114,7 +114,7 @@ exports.fetchPropertyById = async (id) => {
     err.msg = `Property with id ${id} not found.`;
     throw err;
   }
-
+  console.log(query);
   return rows[0];
 };
 
