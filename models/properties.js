009dbf6d9158e36e00f9d1e0d2bfd4bc07c94d6e
ADMIN <admin@iMac.local>
2026-01-23 11:08:15 +0000

    hoing to have solved images multiplying problem

--- models/properties.js
+++ models/properties.js
@@ -86,9 +86,42 @@ exports.fetchProperties = async (filters = {}) => {
   return rows;
 };
 
+// exports.fetchPropertyById = async (id) => {
+//   const query = `
+
+//     SELECT
+//       p.property_id,
+//       p.name AS property_name,
+//       p.location,
+//       p.price_per_night,
+//       p.description,
+//       u.first_name || ' ' || u.surname AS host,
+
+//       COALESCE(ARRAY_AGG(i.image_url ORDER BY i.image_id) FILTER (WHERE i.image_url IS NOT NULL), '{}') AS images,
+//       COUNT(f.favourite_id)::INT AS favourite_count
+//     FROM properties p
+//     JOIN users u ON p.host_id = u.user_id
+//     LEFT JOIN images i ON i.property_id = p.property_id
+//     LEFT JOIN favourites f ON f.property_id = p.property_id
+//     WHERE p.property_id = $1
+//     GROUP BY p.property_id, u.first_name, u.surname;
+//   `;
+
+//   const { rows } = await db.query(query, [id]);
+
+//   if (rows.length === 0) {
+//     const err = new Error(`Property with ID ${id} not found.`);
+//     err.status = 404;
+//     err.msg = `Property with id ${id} not found.`;
+//     throw err;
+//   }
+//   console.log(query);
+//   return rows[0];
+// };
+
 exports.fetchPropertyById = async (id) => {
   const query = `
-            
+
     SELECT
       p.property_id,
       p.name AS property_name,
@@ -96,14 +129,26 @@ exports.fetchPropertyById = async (id) => {
       p.price_per_night,
       p.description,
       u.first_name || ' ' || u.surname AS host,
-      COALESCE(ARRAY_AGG(i.image_url ORDER BY i.image_id) FILTER (WHERE i.image_url IS NOT NULL), '{}') AS images,
-      COUNT(f.favourite_id)::INT AS favourite_count
+
+      COALESCE(img.images, '{}') AS images,
+      COALESCE(fav.favourite_count, 0)::INT AS favourite_count
+
     FROM properties p
     JOIN users u ON p.host_id = u.user_id
-    LEFT JOIN images i ON i.property_id = p.property_id
-    LEFT JOIN favourites f ON f.property_id = p.property_id
-    WHERE p.property_id = $1
-    GROUP BY p.property_id, u.first_name, u.surname;
+
+    LEFT JOIN (
+    SELECT property_id, ARRAY_AGG(image_url ORDER BY image_id) AS images
+    FROM images
+    GROUP BY property_id
+    ) img ON img.property_id = p.property_id
+
+    LEFT JOIN (
+    SELECT property_id, COUNT(*) AS favourite_count
+    FROM favourites
+    GROUP BY property_id
+    ) fav ON fav.property_id = p.property_id
+
+    WHERE p.property_id = $1;
   `;
 
   const { rows } = await db.query(query, [id]);
